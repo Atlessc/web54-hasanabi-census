b3b4c60d152b4d7fd545b33963e9e88ce8be9b02
brilliantdrink <166573010+brilliantdrink@users.noreply.github.com>
2025-09-06 13:13:40 -0700

    holy toledo!!!!!

--- src/components/Slide/HomeSlide.tsx
+++ src/components/Slide/HomeSlide.tsx
@@ -6,8 +6,11 @@ import Signature from '../Signature'
 export default function HomeSlide() {
   return <>
     <div class={cn(styles.slide, styles.home)}>
-      <h1>The HasanAbi Census</h1>
-      <p><i>with data from June 2020 to August 2025</i></p>
+      <h1>
+        The HasanAbi Census
+        <span class={styles.comment}>Holy Toledo!</span>
+      </h1>
+      <p><i>with data from June 2020 to August 2024</i></p>
       {/*<p>art and stylistic direction by Oni_Spumoni</p>*/}
       {/*<p>website by brilliantdrink</p>*/}
       <Signature class={styles.signature} classSvg={''}/>
