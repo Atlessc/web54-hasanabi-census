5c3d1b8eef5a1cca311fdfc3c851e73b1f65ec0d
Tyler S <107712050+Atlessc@users.noreply.github.com>
2026-08-30 15:21:14 -0700

    feat: add stable census category model

--- src/components/Slide/slideData.tsx
+++ src/components/Slide/slideData.tsx
@@ -1,87 +1,137 @@
 import {JSX} from 'solid-js'
 
-import ageFile from "../../data/age.csv"
-import genderFile from "../../data/gender.csv"
-import politicsFile from "../../data/politics.csv"
-import raceFile from "../../data/race.csv"
-import religionFile from "../../data/religion.csv"
-import sexualityFile from "../../data/sexuality.csv"
-import heightFile from "../../data/height.csv"
-import transFile from "../../data/trans.csv"
-import locationFile from "../../data/location.csv"
-import salaryFile from "../../data/salary.csv"
-import educationFile from "../../data/education.csv"
-import sexHaversFile from "../../data/sex-havers.csv"
-import weebFile from "../../data/weeb.csv"
-import eatingFile from "../../data/eating.csv"
-import neurodivergenceFile from "../../data/neurodivergence.csv"
-import gamingFile from "../../data/gaming.csv"
-import watchedFile from "../../data/watched.csv"
+import ageFile from '../../data/age.csv'
+import genderFile from '../../data/gender.csv'
+import politicsFile from '../../data/politics.csv'
+import raceFile from '../../data/race.csv'
+import religionFile from '../../data/religion.csv'
+import sexualityFile from '../../data/sexuality.csv'
+import heightFile from '../../data/height.csv'
+import transFile from '../../data/trans.csv'
+import locationFile from '../../data/location.csv'
+import salaryFile from '../../data/salary.csv'
+import educationFile from '../../data/education.csv'
+import sexHaversFile from '../../data/sex-havers.csv'
+import weebFile from '../../data/weeb.csv'
+import eatingFile from '../../data/eating.csv'
+import neurodivergenceFile from '../../data/neurodivergence.csv'
+import gamingFile from '../../data/gaming.csv'
+import watchedFile from '../../data/watched.csv'
 
 import styles from './slide.module.scss'
-import {colorsAge, colorsHeight} from './colors'
+import {colorsHeight} from './colors'
 
 const madge = 'https://cdn.betterttv.net/emote/6083d2f139b5010444d0540e/3x.webp'
 
-interface SlideData {
-  name: string,
+export type CensusCategoryId =
+  | 'cover'
+  | 'age'
+  | 'gender'
+  | 'political-ideology'
+  | 'ethnicity'
+  | 'religion'
+  | 'sexuality'
+  | 'height'
+  | 'trans-chatters'
+  | 'location'
+  | 'salary'
+  | 'education'
+  | 'vcard'
+  | 'weebs'
+  | 'diet'
+  | 'neurodiversity'
+  | 'gayming-frogs'
+  | 'years-watched'
+
+export interface CensusCategory {
+  id: CensusCategoryId
+  name: string
+}
+
+export interface SlideData extends CensusCategory {
   fileUrl: string
   note?: string | JSX.Element
   colors?: string[]
 }
 
+export const coverCategory: CensusCategory = {
+  id: 'cover',
+  name: 'Cover',
+}
+
 export const barChartSlides: SlideData[] = [{
+  id: 'age',
   name: 'Age',
   fileUrl: ageFile,
-  // colors: colorsAge,
 }, {
+  id: 'gender',
   name: 'Gender',
   fileUrl: genderFile,
 }, {
+  id: 'political-ideology',
   name: 'Political Ideology',
   fileUrl: politicsFile,
 }, {
+  id: 'ethnicity',
   name: 'Ethnicity',
   fileUrl: raceFile,
 }, {
+  id: 'religion',
   name: 'Religion',
   fileUrl: religionFile,
 }, {
+  id: 'sexuality',
   name: 'Sexuality',
   fileUrl: sexualityFile,
 }, {
+  id: 'height',
   name: 'Height',
   fileUrl: heightFile,
   note: <>(Categories changed for 2023 <img src={madge} class={styles.inlineIcon} alt={''} /> )</>,
   colors: colorsHeight,
 }, {
+  id: 'trans-chatters',
   name: 'Trans Chatters',
   fileUrl: transFile,
 }, {
+  id: 'location',
   name: 'Location',
   fileUrl: locationFile,
 }, {
+  id: 'salary',
   name: 'Salary',
   fileUrl: salaryFile,
 }, {
+  id: 'education',
   name: 'Education',
   fileUrl: educationFile,
 }, {
+  id: 'vcard',
   name: 'VCARD',
   fileUrl: sexHaversFile,
 }, {
+  id: 'weebs',
   name: 'Weebs',
   fileUrl: weebFile,
 }, {
+  id: 'diet',
   name: 'Diet',
   fileUrl: eatingFile,
 }, {
+  id: 'neurodiversity',
   name: 'Neurodiversity',
   fileUrl: neurodivergenceFile,
 }, {
+  id: 'gayming-frogs',
   name: 'Gayming Frogs',
   fileUrl: gamingFile,
 }, {
+  id: 'years-watched',
   name: 'Years Watched',
   fileUrl: watchedFile,
 }]
+
+export const censusCategories: CensusCategory[] = [
+  coverCategory,
+  ...barChartSlides.map(({id, name}) => ({id, name})),
+]
