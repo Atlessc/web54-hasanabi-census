ac825efda59128106f1f9cc4ddf7f508a221f4b5
Tyler Smith <tylersmith4596@gmail.com>
2026-08-30 15:26:48 -0700

    Add type declarations for various asset file formats in assets.d.ts

--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -9,3 +9,48 @@ declare module '*.csv' {
   const url: string
   export default url
 }
+
+declare module '*.svg' {
+  const url: string
+  export default url
+}
+
+declare module '*.png' {
+  const url: string
+  export default url
+}
+
+declare module '*.jpg' {
+  const url: string
+  export default url
+}
+
+declare module '*.webp' {
+  const url: string
+  export default url
+}
+
+declare module '*.mp3' {
+  const url: string
+  export default url
+}
+
+declare module '*.txt' {
+  const url: string
+  export default url
+}
+
+declare module '*.ttf' {
+  const url: string
+  export default url
+}
+
+declare module '*.woff' {
+  const url: string
+  export default url
+}
+
+declare module '*.woff2' {
+  const url: string
+  export default url
+}
